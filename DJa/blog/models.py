54c76a46827fb7f25ac953ec459e31a6f5e0865a
Антон <goncharik.a@list.ru>
2021-03-02 16:06:54 +0300

    Category model expanded. Category model registered like app in admin.py.

--- DJa/blog/models.py
+++ DJa/blog/models.py
@@ -6,3 +6,10 @@ class Category(models.Model):
 
     name = models.CharField('Категории', max_length=100)
     slug = models.SlugField('url', max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
