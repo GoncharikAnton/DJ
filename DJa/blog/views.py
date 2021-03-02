7ddb012932cc42b8ba2e4dfeed8157032b995c7e
Антон <goncharik.a@list.ru>
2021-03-02 19:59:58 +0300

    Category model expanded. Category model registered like app in admin.py. Added new templates. New CategoryView.

--- DJa/blog/views.py
+++ DJa/blog/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from .models import *
 
-# Create your views here.
+
+class HomeView(View):
+    """Home page"""
+
+    def get(self, request):
+        category_list = Category.objects.all()
+        return render(request, 'blog/home.html', {'categories': category_list})
+
+
+class CategoryView(View):
+    """Category view"""
+
+    def get(self, request, category_name):
+        category = Category.objects.get(slug=category_name)
+        return render(request, 'blog/post_list.html', {'category': category})
