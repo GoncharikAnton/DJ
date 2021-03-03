86849e753922a3b9f2dbcdeb0caba94ceccf89b0
Антон <goncharik.a@list.ru>
2021-03-03 17:07:36 +0300

    New pre-view: PostView. + new url, changes in home/post_list.html, migration.

--- DJa/blog/urls.py
+++ DJa/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('<slug:category_name>/', views.CategoryView.as_view(), name='category'),
+    # path('<slug:post_title>/', views.PostView.as_view(), name='post'),
     path('', views.HomeView.as_view()),
 ]
--- DJa/blog/views.py
+++ DJa/blog/views.py
@@ -9,12 +9,24 @@ class HomeView(View):
 
     def get(self, request):
         category_list = Category.objects.all()
-        return render(request, 'blog/home.html', {'categories': category_list})
+        post_list = Post.objects.all()
+        page_context = {'categories': category_list, 'post_list': post_list}
+        return render(request, 'blog/home.html', context=page_context)
 
 
 class CategoryView(View):
-    """Category view"""
+    """Category page"""
 
     def get(self, request, category_name):
         category = Category.objects.get(slug=category_name)
         return render(request, 'blog/post_list.html', {'category': category})
+
+#
+# class PostView(View):
+#     """Post page"""
+#
+#     def get(self, request, post_title):
+#         category_list = Category.objects.all()
+#         post = Post.objects.get(slug=post_title)
+#         page_context = {'categories': category_list, 'post': post}
+#         return render(request, 'blog/post_list.html', context=page_context)
