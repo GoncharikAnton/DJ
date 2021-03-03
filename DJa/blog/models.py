38be42edb3f67126ea95e08cef76fcc819e82ac2
Антон <goncharik.a@list.ru>
2021-03-03 16:10:56 +0300

    New models created: Post, Tag, Comment.

--- DJa/blog/models.py
+++ DJa/blog/models.py
@@ -13,3 +13,46 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+class Tag(models.Model):
+    """Tag's model"""
+
+    name = models.CharField('Тэг', max_length=60)
+    slug = models.SlugField('url', max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+
+class Post(models.Model):
+    """Post's model"""
+
+    title = models.CharField('Нахзвание', max_length=250)
+    mini_text = models.TextField('Краткое описание', max_length=400)
+    text = models.TextField('Описание', max_length=2000)
+    created_date = models.DateTimeField('Дата создания', auto_now=True)
+    slug = models.SlugField('url', max_length=250)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+
+class Comment(models.Model):
+    """Comment's model"""
+
+    text = models.TextField('Описание', max_length=750)
+    created_date = models.DateTimeField('Дата создания', auto_now=True)
+    moderation = models.BooleanField('Модерация пройдена', blank=False)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
