5568d415f15da21fd54741a3e3e70d1fa360aba7
Антон <goncharik.a@list.ru>
2021-03-10 20:31:29 +0300

    Combinated Category and Tag views. One URL for Category and Tag.  Changed templates.

--- DJa/blog/urls.py
+++ DJa/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('tag/<slug:tag_slug>/', views.TagView.as_view(), name='tag'),
+    path('tag/<slug:tag_slug>/', views.CategoryView.as_view(), name='tag'),
     path('<slug:category>/<slug:post_slug>', views.PostView.as_view(), name='detail_post'),
     path('<slug:category_slug>/', views.CategoryView.as_view(), name='category'),
     path('', views.HomeView.as_view()),
